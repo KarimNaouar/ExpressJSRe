8b9b1f7e20959a38008dd538e0d5e32642f85d01
KarimNaouar <KarimNaouar@users.noreply.github.com>
2020-09-12 17:46:41 +0200

    ExpressJSRe

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+app.use(date=(req,res,next)=>{
+  let requestDate=new Date();
+  let requestDay= requestDate.getDay();
+  let requestHours= requestDate.getHours();
+  console.log(requestDate,requestDay,requestHours)
+  if(requestDay<=7 && requestHours>=9 && requestHours<=19){
+   console.log('availble')
+   res.sendFile(__dirname + "/routes")
+   }
+  else
+  {
+    console.log('not available')
+    res.sendFile(__dirname + "/routes/error.html")
+  }
+
+})
+
+app.get('/', function(req, res) {
+  console.log("reached root!");
+  res.redirect("/home.html");
+});
+
+app.use(express.static(__dirname + "/routes"));
+
+app.listen(5000, (err)=>{
+  if(err) console.log('server not running')
+	else console.log("server running in port 5000")
+});
